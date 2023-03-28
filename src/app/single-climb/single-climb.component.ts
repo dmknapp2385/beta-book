0926cd77791257966e551dc06ea7d907748f28f8
dmknapp2385 <dmknapp2385@gmail.com>
2023-03-28 15:26:23 -0700

    routes to single climb working

--- src/app/single-climb/single-climb.component.ts
+++ src/app/single-climb/single-climb.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Climb } from '../climb';
+import { ClimbDataService } from '../climb-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-single-climb',
   templateUrl: './single-climb.component.html',
-  styleUrls: ['./single-climb.component.css']
+  styleUrls: ['./single-climb.component.css'],
 })
-export class SingleClimbComponent {
+export class SingleClimbComponent implements OnInit {
+  climb?: Climb;
 
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private climbService: ClimbDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.getClimb();
+  }
+
+  getClimb(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.climbService.getClimb(id).subscribe((climb) => (this.climb = climb));
+  }
 }
