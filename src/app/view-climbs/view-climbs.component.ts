9c5911615a9b4abe40aaccd8bc4a20e3db1ff7f2
dmknapp2385 <dmknapp2385@gmail.com>
2023-03-28 11:57:08 -0700

    basic layout and css for climb list

--- src/app/view-climbs/view-climbs.component.ts
+++ src/app/view-climbs/view-climbs.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Climb } from '../climb';
+import { ClimbDataService } from '../climb-data.service';
 
 @Component({
   selector: 'app-view-climbs',
   templateUrl: './view-climbs.component.html',
-  styleUrls: ['./view-climbs.component.css']
+  styleUrls: ['./view-climbs.component.css'],
 })
 export class ViewClimbsComponent {
+  climbs: Climb[] = [];
+  constructor(private climbService: ClimbDataService) {}
+  ngOnInit(): void {
+    this.getClimbs();
+  }
 
+  getClimbs() {
+    this.climbService.getClimbs().subscribe((climbs) => (this.climbs = climbs));
+  }
+
+  
 }
