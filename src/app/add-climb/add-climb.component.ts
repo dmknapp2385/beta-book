66ed192fbe46080f9ffab40ce888735f7b8dd60d
dmknapp2385 <dmknapp2385@gmail.com>
2023-04-22 16:49:30 -0700

    add climb to database working

--- src/app/add-climb/add-climb.component.ts
+++ src/app/add-climb/add-climb.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { Climb } from '../climb';
+import { Location } from '@angular/common';
+import { ClimbDataService } from '../climb-data.service';
 
 @Component({
   selector: 'app-add-climb',
   templateUrl: './add-climb.component.html',
-  styleUrls: ['./add-climb.component.css']
+  styleUrls: ['./add-climb.component.css'],
 })
 export class AddClimbComponent {
   climb = {
-    name:'', 
-    grade: '', 
-    location: '', 
-    beta: '', 
+    name: '',
+    grade: '',
+    date: new Date(),
+    location: '',
+    beta: '',
+  };
+
+  constructor(
+    private location: Location,
+    private climbService: ClimbDataService
+  ) {}
+  addClimb(): void {
+    this.climbService
+      .addClimb(this.climb as Climb)
+      .subscribe(() => this.goBack());
   }
 
-  
+  goBack(): void {
+    this.location.back();
+  }
 }
