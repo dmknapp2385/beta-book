4e0d86f3b783d38c27c3f6ac297b1c55455738c8
dmknapp2385 <dmknapp2385@gmail.com>
2023-04-24 15:19:15 -0700

    search functionality added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+import { Climb } from '../climb';
+import { ClimbDataService } from '../climb-data.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: [ './search.component.css' ]
 })
-export class SearchComponent {
 
+export class SearchComponent implements OnInit {
+  climbs$!: Observable<Climb[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private climbService: ClimbDataService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.climbs$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.climbService.searchClimbs(term)),
+    );
+  }
 }
