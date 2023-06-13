e532d21fd7c98fa532da6d3fec11c26b7c9ad261
dmknapp2385 <dmknapp2385@gmail.com>
2023-06-13 08:57:24 -0600

    mobile menu styling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // @ViewChild('showbutton') showbutton: ElementRef;
+  // @ViewChild('hidden-menu') menu: ElementRef;
+  constructor(private renderer: Renderer2) {}
+  //hides dropdown initially
+  hidden = true;
+  //shows dropdown nav
+  show = false;
+
   title = 'beta-book';
+
+  //listen for click outside dropdown menu
+
+  //shows
+  showMenu(): void {
+    this.hidden = false;
+    this.show = true;
+  }
+
+  //closes nav bar when selecting destination
+  closeNav(): void {
+    console.log('close nav');
+    this.hidden = true;
+    this.show = false;
+    console.log(this.hidden, this.show);
+  }
 }
